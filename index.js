f4a6e0654430b05ee581ad1885eddae7cef81e44
marcebollin <marceloleon050@gmail.com>
2022-01-24 11:41:29 -0400

    add sweet alert

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ import { DOM } from './modules/DOM.mjs'
 
 const productsURL = 'https://raw.githubusercontent.com/corpchemicals/products-list/main/products.json' 
 
+//Set form
+const checkClientExistence = DOM.get("#toggle-client-existence")
+const newClientFields = document.querySelectorAll(".new-client-field")
+
+checkClientExistence.addEventListener("change", ({target}) => {
+  const container = target.parentElement
+  container.classList.toggle("off-color")
+  container.classList.toggle("on-color")
+
+  newClientFields.forEach(field => {
+    const inputs = field.querySelectorAll("input")
+    
+    inputs.forEach(input => {
+      const wasntRemoved = input.hasAttribute("required")
+      if(wasntRemoved) input.removeAttribute("required")
+      else input.required = true
+    })
+
+    field.classList.toggle("displayNone")
+  })
+})
 fetch(productsURL)
 .then(response => response.json())
 .then(products => {
   const totalOrder = []
   const phoneNumber = '+584244044072'
 
-  //Set form
-  const checkClientExistence = DOM.get("#toggle-client-existence")
-  const newClientFields = document.querySelectorAll(".new-client-field")
-
-  checkClientExistence.addEventListener("change", ({target}) => {
-    const container = target.parentElement
-    container.classList.toggle("off-color")
-    container.classList.toggle("on-color")
-
-    newClientFields.forEach(field => {
-      const inputs = field.querySelectorAll("input")
-      
-      inputs.forEach(input => {
-        const wasntRemoved = input.hasAttribute("required")
-        if(wasntRemoved) input.removeAttribute("required")
-        else input.required = true
-      })
-
-      field.classList.toggle("displayNone")
-    })
-  })
   setSelects(products) 
   setAddButton(totalOrder, products)
   setUlListener(totalOrder)
--- modules/setForm.mjs
+++ modules/setForm.mjs
@@ -134,42 +134,52 @@ export function setSubmitForm(totalOrder, phoneNumber) {
   const form = document.querySelector("form#send-order-form")
   
   form.addEventListener("submit", (ev) => {
+    ev.preventDefault();
     if(totalOrder.length < 1)  {
-      ev.preventDefault();
       return; 
     }
 
-    // Replace for sweetAlert
-    alert("Seguro que quieres enviar el pedido?")
+    Swal.fire({
+      title: '¿Seguro que quieres enviar el pedido?',
+      text: "No podrás realizar cambios después",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, enviar',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const seller = DOM.get("#seller").value
 
-    const seller = DOM.get("#seller").value
-
-    const client = {
-      name: DOM.get("#client-name").value, 
-      phone: DOM.get("#phone-area-code").value +  "-" + DOM.get("#client-phone").value || "----",
-      id: DOM.get("#identification-type").value + "-" + DOM.get("#client-identification").value || "----",
-      address: DOM.get("#client-address").value || "----"
-    }
-
-    let message = 
-    `Vendedor: ${seller} \nCliente: ${client.name} \nTeléfono: ${client.phone} \nIdentificación: ${client.id} \nDirección: ${client.address}\nPedido: \n\n`
+        const client = {
+          name: DOM.get("#client-name").value, 
+          phone: DOM.get("#phone-area-code").value +  "-" + DOM.get("#client-phone").value || "----",
+          id: DOM.get("#identification-type").value + "-" + DOM.get("#client-identification").value || "----",
+          address: DOM.get("#client-address").value || "----"
+        }
     
-    let orderPrice = 0
+        let message = 
+        `Vendedor: ${seller} \nCliente: ${client.name} \nTeléfono: ${client.phone} \nIdentificación: ${client.id} \nDirección: ${client.address}\nPedido: \n\n`
+        
+        let orderPrice = 0
+        
+        for(const order of totalOrder) {
+          const { keyName, amount, uPrice} = order
+          const text = `-${keyName.toUpperCase()}: ${amount} unds. \n`
+          message += text
+          orderPrice += amount * uPrice
+        }
     
-    for(const order of totalOrder) {
-      const { keyName, amount, uPrice} = order
-      const text = `-${keyName.toUpperCase()}: ${amount} unds. \n`
-      message += text
-      orderPrice += amount * uPrice
-    }
-
-    const priceMessage = `Precio Total: ${orderPrice.toFixed(2)}$`
-    message += `\n${priceMessage}`
-
-    const encodedMessage = encodeURI(message)
-    const startedLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=`
-    const url = `${startedLink}${encodedMessage}`
-
-    window.open(url, '_blank')
+        const priceMessage = `Precio Total: ${orderPrice.toFixed(2)}$`
+        message += `\n${priceMessage}`
+    
+        const encodedMessage = encodeURI(message)
+        const startedLink = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=`
+        const url = `${startedLink}${encodedMessage}`
+    
+        window.open(url, '_blank')
+      }
+    })
   })
 }
